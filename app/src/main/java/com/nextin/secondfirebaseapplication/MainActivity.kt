91a2330311982f9ee3df1444972fece7819364d9
kshitij <albatros.29kr@gmail.com>
2024-05-20 20:55:10 +0530

    second Commit

--- app/src/main/java/com/nextin/secondfirebaseapplication/MainActivity.kt
+++ app/src/main/java/com/nextin/secondfirebaseapplication/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.nextin.secondfirebaseapplication
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.core.Tag
+import com.google.firebase.database.database
+import com.google.firebase.database.getValue
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("I am Kshitij")
+
+        myRef.addValueEventListener(object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+               val value =  snapshot.getValue<String>()
+                Toast.makeText(this@MainActivity,value,
+                    Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e(TAG,"Reading data is Failded", error.toException())
+            }
+
+        })
     }
 }
